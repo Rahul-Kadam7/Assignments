f35ab47b10c4a092aede89f44af1b685ca825daf
Rahul-Kadam-7 <Rahulkadam81840@gmail.com>
2025-07-01 12:34:44 +0000

    Initial commit

---
+++ addition.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+a=5
+b=10
+sum=$((a + b))
+echo "The sum of $a and $b is: $sum"
+
---
+++ input.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+read -p "Enter first number: " a
+read -p "Enter second number: " b
+sum=$((a + b))
+echo "The sum of $a and $b is: $sum"
+
